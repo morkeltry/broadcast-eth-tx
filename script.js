ea874cfba43d397ad7e69c2d57e1b35fff234d0c
Malberg <15891590+morkeltry@users.noreply.github.com>
2025-04-16 10:08:44 +0100

    TODO: Add connect button

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Don't want to link your Twitter or do a bunch of CAPTCHAs just to broadcast an EVM tx?
+// Do it through Metamask!
+
 // Run me from console (set Metamask to correct network first)
 
 let rawTx;
@@ -8,6 +11,9 @@ async function broadcastRawTransaction(rawTx) {
     if (typeof window.ethereum === "undefined") {
       throw new Error("MetaMask is not installed.");
     }
+
+    // Need to connct wallet to page manually
+    // TODO: add connect button ;)
 
     // Send the raw transaction using MetaMask's RPC endpoint
     const txHash = await window.ethereum.request({
